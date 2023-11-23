5b26150329d3dd1ef372c8a2b8ce1a889d189e97
Zella Stewart <125938574+zeeebs@users.noreply.github.com>
2023-11-23 11:12:08 -0800

    1st draft complete

---
+++ FlappyBeeVS/FlappyBeeVS/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using static System.Runtime.CompilerServices.RuntimeHelpers;
+
+public class PlayerControlls : MonoBehaviour
+{
+    Rigidbody rigidBody;
+    SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+    void Update()
+    {
+
+
+        if (transform.position.y <= -4.3f)
+        {
+            transform.position = new Vector3(transform.position.x, -4.3f, 0);
+        }
+
+        if (transform.position.x >= 7.5f)
+        {
+            transform.position = new Vector3(7.5f, transform.position.y, 0);
+        }
+
+        else if (transform.position.x <= -7.5f)
+        {
+            transform.position = new Vector3(-7.5f, transform.position.y, 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow)) //left
+        {
+            transform.Translate(-Vector3.right * distance);
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow)) //right
+        {
+            transform.Translate(Vector3.right * distance);
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow)) //down
+        {
+            transform.Translate(-Vector3.up * distance);
+        }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow)) //up
+        {
+            transform.Translate(Vector3.up * distance);
+        }
+
+    }
+
+}
